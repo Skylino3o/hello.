752465288f238afe5917e0fa046943a04f5e6d93
Haonan Sun <hsun17@huskers.unl.edu>
2021-08-31 02:29:55 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,17 +1,19 @@
 /**
  * Author: Chris Bourke
  * Date:   2020
- * 
+ *
  * A simple hello world program
  */
  #include<stdlib.h>
  #include<stdio.h>
- 
+
  int main(int argc, char **argv) {
-     
+
      printf("Haonan Sun!\n");
-     printf("Mathematics")
-     
+     printf("Major: Mathematics\");
+     printf("Sonya Wu!\n");
+     printf("Major: Mathematics\n");
+
      return 0;
-     
-}git
+
+}
