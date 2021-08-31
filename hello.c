6363b2d1b5a5b932a0c10d66e0f3f859f2319b6a
Haonan Sun <hsun17@huskers.unl.edu>
2021-08-31 01:10:22 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Chris Bourke
+ * Date:   2020
+ * 
+ * A simple hello world program
+ */
+ #include<stdlib.h>
+ #include<stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Hello world!\n");
+     
+     return 0;
+     
+}
